9c974bc68a24f1e7355851cb00d9fd78ad257442
geothomas <geothomas@cet.ac.in>
2020-11-29 11:59:09 +0530

    route added

--- SampleServer.js
+++ SampleServer.js
@@ -1,6 +1,24 @@
 var http=require('http')
+var fs=require('fs')
+
 http.createServer(function (req,res)
 {
-    res.write("Test Server")
+    if(req.url==='/')
+    {
+    fs.readFile('samplehtml.html',function (err,data){
+        res.writeHead(200,{'Content-Type':'text/html'})
+        res.write(data)
+        res.end()
+    })
+} else if(req.url==='/login')
+{
+    res.write("login")
     res.end()
-}).listen(7001)
+}else
+{
+    res.write("Error")
+    res.end()
+}
+}).listen(7001,function (){
+    console.log("Server start Running let start to do ...................")
+})
