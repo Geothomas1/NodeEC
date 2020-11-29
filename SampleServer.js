9385e7ab60dbd991e80c052c6dc727cfa729d57f
geothomas <geothomas@cet.ac.in>
2020-11-29 13:29:53 +0530

    Server routing

--- SampleServer.js
+++ SampleServer.js
@@ -1,16 +1,19 @@
 var http=require('http')
 var fs=require('fs')
+var url=require('url')
 
 http.createServer(function (req,res)
 {
-    if(req.url==='/')
+    var q=url.parse(req.url,true)
+    //console.log(q.pathname)
+    if(q.pathname==='/')
     {
     fs.readFile('samplehtml.html',function (err,data){
         res.writeHead(200,{'Content-Type':'text/html'})
         res.write(data)
         res.end()
     })
-} else if(req.url==='/signup')
+} else if(q.pathname==='/signup')
 {
     fs.readFile('signup.html',(err,data)=>{
         res.writeHead(200,{'Content-Type':'text/html'})
@@ -18,9 +21,10 @@ http.createServer(function (req,res)
         res.end()
     })
     
-}else if(req.url==='/signupaction')
+}else if(q.pathname==='/signupaction')
 {
-  
+    res.writeHead(200,{'Content-Type':'text/html'})
+    res.write("<h1>"+q.query.name+"</h1>")
     res.write("From Data submitted")
     res.end()
 }
