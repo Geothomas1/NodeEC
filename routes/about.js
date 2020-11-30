05c9841b624aaad7b9b3c1bcb0a548459a841712
geothomas <geothomas@cet.ac.in>
2020-11-30 10:18:20 +0530

    about router added

--- routes/about.js
+++ routes/about.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'github' });
+  res.render('about', { value: 'You Are in About Page' });
 });
 
 module.exports = router;
