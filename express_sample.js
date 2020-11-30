6c8235e938ebdbd95c6b24cb5f21d69e5417f3e0
geothomas <geothomas@cet.ac.in>
2020-11-30 07:10:22 +0530

    express started 2

--- express_sample.js
+++ express_sample.js
@@ -4,6 +4,9 @@ app.get('/',function (req,res){
     res.send("Hello")
 })
 app.get('/about',(req,res)=>{
-    res.send("About page")
+    res.sendFile('signup.html')
 })
-app.listen(7000,()=>console.log("Server started......"))
+app.listen(7000,()=>{
+    console.log("Server started......")
+console.log(__dirname)
+})
