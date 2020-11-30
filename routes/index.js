f6f524c399ab30bc73a39e9f43c0b15ab75e9db8
geothomas <geothomas@cet.ac.in>
2020-11-30 10:58:38 +0530

    if in hbs block from index.js via perons and value data struct

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,8 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
   const values=['Geo','Manuel','Faizal','Febin','Anna','Pranoy','Anajan','Jitho','Akash']
-  res.render('index', {values});
+  const person={name:"Geo",admin:true,tcomment:"This is a valid comment",fcomment:"This is not a valid comment"}
+  res.render('index', {values,person});
 });
 
 module.exports = router;
