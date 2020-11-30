b07c2aededa25577ad7731430cf5a6db14a5bb2e
geothomas <geothomas@cet.ac.in>
2020-11-30 11:10:41 +0530

    hbs basics ok

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
   const values=['Geo','Manuel','Faizal','Febin','Anna','Pranoy','Anajan','Jitho','Akash']
-  const person={name:"Geo",admin:true,tcomment:"This is a valid comment",fcomment:"This is not a valid comment"}
+  const person={name:"Geo",admin:true,tcomment:"This is a valid comment",fcomment:"This is not a valid comment",data:{comment1:"You are in College no class today",comment2:"You are not in college have class today"}}
   res.render('index', {values,person});
 });
 
