e08c249ee3f10f74548e7d8daf98da4feff61491
geothomas <geothomas@cet.ac.in>
2020-11-30 07:13:24 +0530

    express path

--- express_sample.js
+++ express_sample.js
@@ -1,12 +1,15 @@
 const express=require('express')
 const app=express()
+const path=require('path')
+
 app.get('/',function (req,res){
     res.send("Hello")
 })
 app.get('/about',(req,res)=>{
-    res.sendFile('signup.html')
+    res.sendFile(path.join(__dirname,'signup.html'))
 })
 app.listen(7000,()=>{
     console.log("Server started......")
 console.log(__dirname)
+console.log(__filename)
 })
