5bbe67ba08c1c39c821e395e5f27ac4478ffedc1
geothomas <geothomas@cet.ac.in>
2020-11-30 07:07:17 +0530

    express started 1

--- express_sample.js
+++ express_sample.js
@@ -2,6 +2,8 @@ const express=require('express')
 const app=express()
 app.get('/',function (req,res){
     res.send("Hello")
-
+})
+app.get('/about',(req,res)=>{
+    res.send("About page")
 })
 app.listen(7000,()=>console.log("Server started......"))
