a80983cd2836b9acb624154e5b8fb8e44aab5f18
geothomas <geothomas@cet.ac.in>
2020-12-01 07:52:52 +0530

    All are going good using promise

--- Sampleservercode/Promis-sample.js
+++ Sampleservercode/Promis-sample.js
@@ -1,3 +1,4 @@
+//This is by simple callback process but it is difficult to understand after trace back the code this can solved by promise
 function add(num1,num2,callback){
     let err=false
     if(num1==0){
@@ -30,3 +31,44 @@ muliplay(sum,sum,(product)=>{
 })
 }
 })
+
+//Callback using Promise
+
+const Promise=require('promise')
+function add(num1,num2){
+    return new Promise((resolve,reject)=>{
+        //just show sample reject flow if num1=0 consider as error case 
+        if(num1==0)
+        {
+            reject("ERROR")
+        }
+        resolve(num1+num2)
+    })
+
+}
+function muliplay(num1,num2){
+    return new Promise((resolve,reject)=>{
+        resolve(num1*num2)
+    })
+
+}
+function division(num1,num2){
+    return new Promise((resolve,reject)=>{
+        resolve(num1/num2)
+    })
+
+}
+add(10,20).then((sum)=>{
+    console.log(sum)
+    return muliplay(sum,sum)
+}).then((product)=>{
+    console.log(product)
+    return division(product,10)
+}).then((result)=>{
+    console.log(result)
+})
+.catch((err)=>{
+    console.log(err)
+})
+
+//this is called chaining process by promise
